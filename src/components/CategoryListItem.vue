225f4914c17ce4e4bb61193f9d76e22b28c0a9eb
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-11-27 19:29:01 +0100

    Categories, Collection of Forum

--- src/components/CategoryListItem.vue
+++ src/components/CategoryListItem.vue
@@ -11,12 +11,24 @@
 
 <script>
 import ForumList from '@/components/ForumList'
+import sourceData from '@/data.json'
 
 export default {
+  components: {
+    ForumList
+  },
+
   props: {
     category: {
       required: true,
       type: Array
+    }
+  },
+
+  computed: {
+    categoryForums () {
+      return Object.values(sourceData.forums)
+        .filter(forum => forum.categoryID === this.category['.key'])
     }
   }
 }
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,27 +1,24 @@
 <template>
 <div class="col-full">
   <h1>Welcome to the Forum</h1>
-  <ForumList :forums="forums"/>
-
+  <CategoryList :categories="categories"/>
 </div>
 </template>
 
 <script>
-// @ er src
+// @ er src fordi det er defineret i webpack
 import sourceData from '@/data.json'
-import ForumList from '@/components/ForumList'
+import CategoryList from '@/components/CategoryList'
 console.log(sourceData)
 
 export default {
   components: {
-    ForumList
+    CategoryList
   },
 
   data () {
     return {
-      forums: Object.values(sourceData.forums),
-      posts: sourceData.posts,
-      users: sourceData.users
+      categories: Object.values(sourceData.categories)
     }
   }
 }
